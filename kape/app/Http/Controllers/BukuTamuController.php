30b37769c5dcd28ee4e0d9a34f9d406d53589cc8
totti-love <ridhodwijayanto_2225110005@mhs.mdp.ac.id>
2024-09-17 18:38:44 +0700

    migration

---
+++ kape/app/Http/Controllers/BukuTamuController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BukuTamu;
+use Illuminate\Http\Request;
+
+class BukuTamuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BukuTamu $bukuTamu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BukuTamu $bukuTamu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BukuTamu $bukuTamu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BukuTamu $bukuTamu)
+    {
+        //
+    }
+}
---
+++ kape/database/migrations/2024_09_17_101334_create_buku_tamus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buku_tamus', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->time('jam_datang');
+            $table->time('jam_pulang');
+            $table->string('nama');
+            $table->char('foto');
+            $table->char('tujuan');
+            $table->char('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku_tamus');
+    }
+};
